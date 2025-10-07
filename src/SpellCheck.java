3f7ac9fe91ed35e27e11c10ab43d197964ec9ae5
lkassaei <lily.kassaei@menloschool.org>
2025-10-06 19:09:53 -0700

    Finished SpellCheck v1.5

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -17,7 +17,78 @@ public class SpellCheck {
      * @param dictionary The list of all accepted words.
      * @return String[] of all mispelled words in the order they appear in text. No duplicates.
      */
-    // First code the trie: Need trie class (Node root, Fncs: insert(), lookup()) and Node class (boolean isTerminal, array of Nodes to children. Fncs: isWord() setWord() getNext()
+    // First code the trie: Need trie class
+    // (Node root, Fncs: insert(), lookup()) and
+    // Node class (boolean isTerminal, array of Nodes to children. Fncs: isWord() setWord() getNext()
+    // HYPHENS
+
+    // Then for the final solution code the TST (tertiary search trie)
+    public class Node {
+        private boolean isTerminal;
+        private Node[] children;
+
+        public Node() {
+            this.isTerminal = false;
+            this.children = new Node[256];
+        }
+
+        public boolean isWord() {
+            return this.isTerminal;
+        }
+
+        public void setWord() {
+            this.isTerminal = true;
+        }
+
+        public Node getNext(char c) {
+            if (c < 0 || c >= 256) {
+                return null;
+            }
+            return this.children[c];
+        }
+
+        public void setNext(char c, Node node) {
+            if (c >= 0 && c < 256) {
+                this.children[c] = node;
+            }
+        }
+    }
+
+    public class Trie {
+        private Node root;
+
+        public Trie() {
+            this.root = new Node();
+        }
+
+        public void insert(String word) {
+            Node current = root;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                Node next = current.getNext(c);
+                if (next == null) {
+                    next = new Node();
+                    current.setNext(c, next);
+                }
+                current = next;
+            }
+            current.setWord();
+        }
+
+        public boolean lookup(String word) {
+            Node current = root;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                current = current.getNext(c);
+
+                if (current == null) {
+                    return false;
+                }
+            }
+            return current.isWord();
+        }
+    }
+
     // Trie Pseudocode:
     // Trie for words in dict
     // Trie for misspelled words
@@ -25,36 +96,20 @@ public class SpellCheck {
     //      if not in either mispelled or dict:
     //          add to misspelled
     // Return num of misspelled
-
-    // Then for the final solution code the TST (tertiary search trie)
     public String[] checkWords(String[] text, String[] dictionary) {
-        ArrayList<String> visited = new ArrayList<>();
-        ArrayList<String> words = new ArrayList<>();
-        for (int i = 0; i < text.length; i ++) {
-            if (!binarySearch(text[i], dictionary) && !visited.contains(text[i])) {
-                words.add(text[i]);
-                visited.add(text[i]);
-            }
+        Trie words = new Trie();
+        for (String word : dictionary) {
+            words.insert(word);
         }
-        return words.toArray(new String[0]);
-    }
+        ArrayList<String> misspelled = new ArrayList<>();
+        Trie seenMisspelled = new Trie();
 
-    public boolean binarySearch(String word, String[] dictionary) {
-        int low = 0;
-        int high = dictionary.length - 1;
-        while (low <= high) {
-            int mid = (low + high)/2;
-            int comparison = word.compareTo(dictionary[mid]);
-            if (comparison == 0) {
-                return true;
-            }
-            else if (comparison < 0) {
-                high = mid - 1;
-            }
-            else {
-                low = mid + 1;
+        for (String word : text) {
+            if (!words.lookup(word) && !seenMisspelled.lookup(word)) {
+                misspelled.add(word);
+                seenMisspelled.insert(word);
             }
         }
-        return false;
+        return misspelled.toArray(new String[0]);
     }
 }
